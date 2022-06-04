a0893f5f58b46fce174b9712310a90b050f9ee7e
Roshani Shrestha <roshanishrestha983@gmail.com>
2022-06-04 19:06:44 -0400

    update

--- app/__init__.py
+++ app/__init__.py
@@ -9,6 +9,14 @@ def home():
     '''Displays the page where the user can start a game.'''
     return render_template('home.html')
 
+def listnames():
+    displaynames = []
+    with open("displaynames.txt") as seenames:
+        reader = csv.reader(seenames)
+        for row in reader:
+            displaynames.append(row[0])
+    return displaynames
+    
 @app.route("/game")
 def game():
     '''Actual game page.'''
@@ -21,45 +29,37 @@ def game():
     # print(randelement)
     temp = []
     temp.append(randelement)
-    with open("displaynames.txt", 'w') as dnames:
+    with open("displaynames.txt", 'a') as dnames:
         fwriter = csv.writer(dnames)
         fwriter.writerow(temp)
-    displaynames = []
-    with open("displaynames.txt") as seenames:
-        reader = csv.reader(seenames)
-        for row in reader:
-            displaynames.append(row)
-    return render_template('game.html', display=displaynames[0])
+    displaynames = listnames() 
+    return render_template('game.html', display=displaynames)
 
 @app.route("/remember")
 def remember():
     '''Displays page so that the user can enter the names they remember.'''
-    return render_template('remember.html')
+    displaynames = listnames() 
+    return render_template('remember.html', num=len(displaynames))
 
 @app.route("/answer", methods=["GET", "POST"])
 def answer():
     '''Checks if it is correct.'''
-    displaynames = []
-    with open("displaynames.txt") as seenames:
-        reader = csv.reader(seenames)
-        for row in reader:
-            displaynames.append(row)
+    displaynames = listnames()
     try:
-        names = []
-        answer = request.args.get('ans')
-        print(answer)
-        if ',' in answer:
-            names = answer.split(",")
-        else:
-            names.append(answer) 
-        print(names)
-        print(displaynames)
-        if names == displaynames[0]:
+        num = str(len(displaynames))
+        answer = request.args.get(num).strip()
+        # print(answer)
+        names = answer.split(",")
+        # print(names)
+        # print(displaynames)
+        # print(names != displaynames)
+        if names == displaynames:
             return redirect("/game")
         else:
-            message = "Your score is " + (displaynames.len() - 1)
-            with open("displaynames.txt", 'r') as delnames:
-                delnames.truncate()
+            message = "Your score is " + str(len(displaynames) - 1)
+            f = open("displaynames.txt", "a")
+            f.truncate(0)
+            f.close()
             return render_template('home.html', msg=message)
     except:
         message = "Sorry, an unknown error occurred. Please try again."
